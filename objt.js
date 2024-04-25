e735539e35b99ceb5457ebf6a0f5b4445e57c865
IsaPalandrani <143020084+IsaPalandrani@users.noreply.github.com>
2024-04-25 12:17:42 -0300

    Add files via upload

---
+++ objt.js
@@ -0,0 +1,28 @@
+class hoje{
+    constructor(hoje){
+        this.hojee = hoje;
+    }
+    queDiaEHoje(){
+        return this.hojee
+}
+}
+
+class horas{
+    constructor(horario){
+        this.horarioAtual = horario;
+    }
+
+    queHorasSao(){
+        return this.horarioAtual;
+    }
+}
+
+class temperatura{
+    constructor(temperatura){
+        this.temperaturaAtual = temperatura;
+    }
+
+    qualATemperatura(){
+        return this.horarioAtual;
+    }
+}
